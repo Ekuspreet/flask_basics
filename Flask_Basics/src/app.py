9e65fa2df94ccc78b3f1db9910a7d99583907638
Ekuspreet <ekuspreetsingh2@gmail.com>
2023-09-22 22:16:38 +0530

    Work done

---
+++ Flask_Basics/src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+username = ""
+
+@app.route("/", methods = ['POST','GET'])
+def welcomeuser():
+    return render_template('landing.html')
+
+@app.route("/base", methods = ['GET','POST'])
+def base():
+    render_template('base.html')
+
+@app.route("/homepage")
+def welcomeadmin( username):
+    return username
+
+@app.route("/welcome/<name>")
+def welcome(name):
+    if name != 'admin':
+        return redirect(url_for('welcomeuser', username=name))
+    else:
+        return redirect(url_for('welcomeadmin'))
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 8000)
